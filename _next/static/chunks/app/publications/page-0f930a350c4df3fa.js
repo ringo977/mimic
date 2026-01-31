2829caca56dfce78a76d1c2a44da6186bf55a450
Marco Rasponi <marco@192.168.1.14>
2026-01-31 09:11:50 +0100

    Updates

--- _next/static/chunks/app/publications/page-0f930a350c4df3fa.js
+++ _next/static/chunks/app/publications/page-0f930a350c4df3fa.js
@@ -3,4 +3,9 @@
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */let i=(0,l(7461).Z)("ExternalLink",[["path",{d:"M15 3h6v6",key:"1q9fwt"}],["path",{d:"M10 14 21 3",key:"gplh6r"}],["path",{d:"M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6",key:"a6xqqp"}]])}},function(e){e.O(0,[650,228,971,69,744],function(){return e(e.s=3473)}),_N_E=e.O()}]);
+ */let i=(0,l(7461).Z)("ExternalLink",[["path",{d:"M15 3h6v6",key:"1q9fwt"}],["path",{d:"M10 14 21 3",key:"gplh6r"}],["path",{d:"M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6",key:"a6xqqp"}]])},9475:function(e,t,l){"use strict";l.d(t,{Z:function(){return i}});/**
+ * @license lucide-react v0.323.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let i=(0,l(7461).Z)("FileText",[["path",{d:"M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z",key:"1rqfz7"}],["path",{d:"M14 2v4a2 2 0 0 0 2 2h4",key:"tnqrlb"}],["path",{d:"M10 9H8",key:"b1mrlr"}],["path",{d:"M16 13H8",key:"t4e002"}],["path",{d:"M16 17H8",key:"z1uh3a"}]])}},function(e){e.O(0,[650,471,971,69,744],function(){return e(e.s=3473)}),_N_E=e.O()}]);
